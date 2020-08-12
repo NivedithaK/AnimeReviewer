f540cf2157a24baaf6af93a524af07efdca9cf89
Nive <nivedithakani@gmail.com>
2020-08-12 19:45:16 -0400

    Change port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
