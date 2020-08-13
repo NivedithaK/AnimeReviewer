8439eb807b905e47307667a1f06feb4e4d12cb15
Nive <nivedithakani@gmail.com>
2020-08-12 20:07:13 -0400

    Fixed typo

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
